d49f152dd862b16800cae278e07271c0da925114
Javier Fernandez <Dogsouldev@gmail.com>
2022-06-15 16:23:51 +0200

    add some other minor fix

--- views/employee/employee.php
+++ views/employee/employee.php
@@ -4,8 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Employee In</title>
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <script src="../../assets/js/script.js" type="module"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <title>Employee In</title>
 </head>
 
 <body>
