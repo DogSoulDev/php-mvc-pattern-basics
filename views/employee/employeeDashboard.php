0ce98ad6ffd8af96e0007636f925675748aaf2fa
Javier Fernandez <Dogsouldev@gmail.com>
2022-06-16 09:01:29 +0200

    add normalize and fix stuff

--- views/employee/employeeDashboard.php
+++ views/employee/employeeDashboard.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="../../assets/css/normalize.css.css">
     <script src="../../assets/js/script.js" type="module"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <title>Employee Dash</title>
